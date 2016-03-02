1925354f8e628d1e62742ce3f220f487c4a83e66
Ivan Gropetti <ivan.gropetti@gmail.com>
2016-03-02 10:52:01 +0100

    Update Home.js

--- OfficeAdd-ins/ExcelAddIns/001_Tables/Home.js
+++ OfficeAdd-ins/ExcelAddIns/001_Tables/Home.js
@@ -54,7 +54,7 @@
         var Httpreq = new XMLHttpRequest();
         //Httpreq.setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
         Httpreq.open("GET",yourUrl,false);
-        Httpreq.setRequestHeader("Access-Control-Allow-Origin", "*");
+        //Httpreq.setRequestHeader("Access-Control-Allow-Origin", "*");
         Httpreq.send(null);
         return Httpreq.responseText;
 
