ba83b9e6d7bc8b16da02e05e924e288bde3d995d
Ivan Gropetti <ivan.gropetti@gmail.com>
2016-03-04 07:50:50 +0100

    Update Home.js

--- OfficeAdd-ins/ExcelAddIns/001_Tables/Home.js
+++ OfficeAdd-ins/ExcelAddIns/001_Tables/Home.js
@@ -33,9 +33,9 @@
     //Function that takes all the opened Banana documents and creates a list
     function getFileNamesList()
     {
-        var jsonFilenameObj = JSON.parse(Get("http://localhost:8081/v1/docs"));
+        //var jsonFilenameObj = JSON.parse(Get("http://localhost:8081/v1/docs"));
         //var jsonFilenameObj = JSON.parse(Get("http://localhost/ProvaCrossDomain/prova.json"));
-        //var jsonFilenameObj = JSON.parse(Get("https://raw.githubusercontent.com/BananaAccounting/General/master/OfficeAdd-ins/ExcelAddIns/001_Tables/test.json"));
+        var jsonFilenameObj = JSON.parse(Get("https://raw.githubusercontent.com/BananaAccounting/General/master/OfficeAdd-ins/ExcelAddIns/001_Tables/test.json"));
         var len = jsonFilenameObj.length;
 
         for (var i = 0; i < len; i++)
