5a4644c9332d1fbfe6c56f197ae7eb069e241b05
Ivan Gropetti <ivan.gropetti@gmail.com>
2017-01-11 09:02:42 +0100

    Added GPX import apps

---
+++ ImportApps/GPX/from_table_to_gpx_file/Route/route_gpx.js
@@ -0,0 +1,82 @@
+// Copyright [2015] [Banana.ch SA - Lugano Switzerland]
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// @id = ch.banana.app.routegpx
+// @api = 1.0
+// @pubdate = 2017-01-10
+// @publisher = Banana.ch SA
+// @description = Create a .gpx Route file starting from a table in Banana
+// @task = export.file
+// @doctype = *.*
+// @docproperties = 
+// @outputformat = none
+// @inputdatasource = none
+// @timeout = -1
+// @exportfiletype = gpx
+
+
+
+function exec() {
+
+	var xml = '';
+	xml += '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
+	xml += '\n' + '<gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="RouteConverter">';
+
+	xml = add_metadata(xml);
+	xml = add_rte(xml);
+
+	xml += '\n' + '</gpx>';
+
+	return xml;
+}
+
+function add_metadata(xml) {
+
+	var metadataName = 'Test file Banana.ch SA ....';
+
+	xml += '\n' + '\t' + '<metadata>';
+	xml += '\n' + '\t' + '\t' + '<name>' + metadataName + '</name>';
+	xml += '\n' + '\t' + '</metadata>';
+
+	return xml;
+}
+
+
+function add_rte(xml) {
+
+	var rte_name = "Banana's Route";
+
+	xml += '\n' + '\t' + '<rte>';
+	xml += '\n' + '\t' + '\t' + '<name>' + rte_name + '</name>';
+
+	var table = Banana.document.table('GPX');
+	
+	for (var i = 0; i < table.rowCount; i++) {
+		var tRow = table.row(i);
+		var lon = tRow.value("Lon");
+		var lat = tRow.value("Lat");
+		var ele = tRow.value("Ele");
+		var name = tRow.value("Name");
+	
+		xml += '\n' + '\t' + '\t' + '<rtept lon="'+ lon + '" lat="' + lat + '">';
+		xml += '\n' + '\t' + '\t' + '\t' + '<ele>' + ele + '</ele>';
+		xml += '\n' + '\t' + '\t' + '\t' + '<name>' + name + '</name>';
+		xml	+= '\n' + '\t' + '\t' + '</rtept>';
+	}
+
+	xml += '\n' + '\t' + '</rte>';
+
+	return xml;
+}
+
