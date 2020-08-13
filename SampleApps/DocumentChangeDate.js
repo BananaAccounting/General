060d5f812adcebadd36658620bf83b891a4e98c7
Claudia Albisetti <claudia2@banana.ch>
2020-08-13 15:14:47 +0200

    Update DocumentChangeDate.js

--- SampleApps/DocumentChangeDate.js
+++ SampleApps/DocumentChangeDate.js
@@ -25,30 +25,31 @@
 function exec(inData, options) {
 
     var param = initParam();
+    Banana.console.debug(JSON.stringify(param));
     var ischange = false;
 
     var documentChange = { "format": "documentChange", "error": "", "data": [] };
 
-    if (param.accountingopenyear.length > 0) {
-        var jsonDoc = setfileInfo("AccountingDataBase", "OpeningDate", param.accountingopenyear);
+    if (param.newaccountingopeningdate.length > 0) {
+        var jsonDoc = setfileInfo("AccountingDataBase", "OpeningDate", param.newaccountingopeningdate);
         documentChange["data"].push(jsonDoc);
         ischange = true;
     }
 
-    if (param.accountingcloseyear.length > 0) {
-        jsonDoc = setfileInfo("AccountingDataBase", "ClosureDate", param.accountingcloseyear);
+    if (param.newaccountingclosuredate.length > 0) {
+        jsonDoc = setfileInfo("AccountingDataBase", "ClosureDate", param.newaccountingclosuredate);
         documentChange["data"].push(jsonDoc);
         ischange = true;
     }
 
-    if (param.accountingheaderleft.length > 0) {
-        jsonDoc = setfileInfo("Base", "HeaderLeft", param.accountingheaderleft);
+    if (param.newaccountingheaderleft.length > 0) {
+        jsonDoc = setfileInfo("Base", "HeaderLeft", param.newaccountingheaderleft);
         documentChange["data"].push(jsonDoc);
         ischange = true;
     }
 
-    if (param.accountingheaderright.length > 0) {
-        jsonDoc = setfileInfo("Base", "HeaderRight", param.accountingheaderright);
+    if (param.newaccountingheaderright.length > 0) {
+        jsonDoc = setfileInfo("Base", "HeaderRight", param.newaccountingheaderright);
         documentChange["data"].push(jsonDoc);
         ischange = true;
     }
@@ -71,51 +72,73 @@ function exec(inData, options) {
     }
 }
 
+/** 
+ * controllo che l'anno degli header corrsiponda a quello della data di apertura, se corrisponde, allora ce una data e l'aggiorno con quella attuale,altrimenti ce altro
+ * controllo che l'anno di apertura e di chiusura abbiano l'anno corrente, se non e cosi gli aggiorno con l'anno corrente
+ */
 function initParam() {
     var param = {};
+    param.differenceyear = 0;
     param.accountingyear = "";
-    param.newaccountingyear = "";
-    param.accountingopenyear = "";
-    param.accountingcloseyear = "";
-    param.accountingheaderleft = "";
-    param.accountingheaderright = "";
+    param.newaccountingyear = new Date().getFullYear();
+    param.newaccountingopeningdate = "";
+    param.newaccountingclosuredate = "";
+    param.newaccountingheaderleft = "";
+    param.newaccountingheaderright = "";
 
-    //anno corrente
-    var currentDate = new Date();
-    var currentyear = currentDate.getFullYear();
-    param.newaccountingyear = currentyear;
-
-    //nuova data d'apertura del documento
-    param.accountingopenyear = currentyear + "0101";
-
-    //nuova data di chiusura del documento
-    param.accountingcloseyear = currentyear + "1231";
-
-
-    //controllo gli header
 
     var OpeningDate = Banana.document.info("AccountingDataBase", "OpeningDate");
     if (OpeningDate && OpeningDate.length > 4) {
         param.accountingyear = OpeningDate.toString().substr(0, 4);
         var Headerleft = Banana.document.info("Base", "HeaderLeft");
         if (Headerleft && Headerleft.indexOf(param.accountingyear) >= 0) {
-            Headerleft = Headerleft.replace(param.accountingyear, currentyear);
-            param.accountingheaderleft = Headerleft;
+            Headerleft = Headerleft.replace(param.accountingyear, param.newaccountingyear);
+            param.newaccountingheaderleft = Headerleft;
 
         }
         var Headerright = Banana.document.info("Base", "HeaderRight");
         if (Headerright && Headerright.indexOf(param.accountingyear) >= 0) {
-            Headerright = Headerright.replace(param.accountingyear, currentyear);
-            param.accountingheaderright = Headerright;
+            Headerright = Headerright.replace(param.accountingyear, param.newaccountingyear);
+            param.newaccountingheaderright = Headerright;
         }
 
-    } else {
-        param.accountingopenyear = "";
-        param.accountingcloseyear = "";
+        var currentYearint = parseInt(param.newaccountingyear);
+        var fileYearint = parseInt(param.accountingyear);
 
+        param.differenceyear = Banana.SDecimal.subtract(currentYearint, fileYearint);
+
+        if (parseInt(param.differenceyear) != 0) {
+            param.newaccountingopeningdate = param.newaccountingyear.toString() + OpeningDate.toString().substr(4);
+            param.newaccountingopeningdate = param.newaccountingopeningdate.replace(/-/g, "");
+            var closureDate = Banana.document.info("AccountingDataBase", "ClosureDate");
+            if (closureDate && closureDate.length > 4) {
+                var year = closureDate.toString().substr(0, 4);
+                var newyear = parseInt(year) + parseInt(param.differenceyear);
+                param.newaccountingclosuredate = newyear.toString() + closureDate.toString().substr(4);
+                param.newaccountingclosuredate = param.newaccountingclosuredate.replace(/-/g, "");
+
+            }
+
+        }
     }
-
     return param;
+}
+
+/**¨
+ * prende come parametri la data corrente e param per utilizzare i parametri inizializzati nell init param
+ * se l'anno dell opening date e l'anno corrente sono uguali ritorno vuoto, perche si fa conto che le date scritte
+ * rispecchino gia quella di apertura attuale. se l'anno  e diverso, aggiorno l'anno, ricorstruisco la data aggiornata e la ritorno
+ */
+function getNewRowDate(currentDate, param) {
+    if (!currentDate || currentDate.length < 4)
+        return "";
+    if (param.differenceyear == 0)
+        return "";
+    var currentyear = currentDate.substr(0, 4);
+    var newyear = parseInt(currentyear) + parseInt(param.differenceyear);
+    var newDate = newyear.toString() + currentDate.substr(4);
+
+    return newDate;
 }
 
 function getCurrentDate() {
@@ -174,7 +197,6 @@ function setfileInfo(key1, key2, value) {
 }
 
 function setTransactionsDate(param) {
-
     var table = Banana.document.table("Transactions");
     if (!table) {
         return;
@@ -183,25 +205,28 @@ function setTransactionsDate(param) {
 
     for (var i = 0; i < table.rowCount; i++) {
         var tRow = table.row(i)
-        var TransYear = tRow.value('Date');
-        if (TransYear && TransYear.length > 4) {
-            TransYear = param.newaccountingyear.toString() + TransYear.substr(4);
+        var TransDate = tRow.value('Date');
+        TransDate = getNewRowDate(TransDate, param);
+        if (TransDate.length <= 0) {
 
-            //row operation
-            var row = {};
-            row.operation = {};
-            row.operation.name = "modify";
-            row.operation.sequence = i.toString();
-
-            row.fields = {};
-            row.fields["Date"] = TransYear;
-
-
-            rows.push(row);
+            continue;
         }
+
+        //row operation
+        var row = {};
+        row.operation = {};
+        row.operation.name = "modify";
+        row.operation.sequence = i.toString();
+
+        row.fields = {};
+        row.fields["Date"] = TransDate;
+
+
+        rows.push(row);
     }
     if (rows.length <= 0)
         return;
+
 
 
     //table
@@ -228,23 +253,28 @@ function setBudgetDate(param) {
     var rows = [];
 
     for (var i = 0; i < table.rowCount; i++) {
-        var tRow = table.row(i)
-        var TransYear = tRow.value('Date');
-        if (TransYear && TransYear.length > 4) {
-            TransYear = param.newaccountingyear.toString() + TransYear.substr(4);
+        var tRow1 = table.row(i)
+        var TransDate = tRow1.value('Date');
+        var TransDateEnd = tRow1.value('DateEnd');
+        TransDate = getNewRowDate(TransDate, param);
+        TransDateEnd = getNewRowDate(TransDateEnd, param);
+        if (TransDate.length <= 0) {
 
-            //row operation
-            var row = {};
-            row.operation = {};
-            row.operation.name = "modify";
-            row.operation.sequence = i.toString();
-
-            row.fields = {};
-            row.fields["Date"] = TransYear;
-
-
-            rows.push(row);
+            continue;
         }
+
+        //row operation
+        var row = {};
+        row.operation = {};
+        row.operation.name = "modify";
+        row.operation.sequence = i.toString();
+
+        row.fields = {};
+        row.fields["Date"] = TransDate;
+        row.fields["DateEnd"] = TransDateEnd;
+
+
+        rows.push(row);
     }
     if (rows.length <= 0)
         return;
