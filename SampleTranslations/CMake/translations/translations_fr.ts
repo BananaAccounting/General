bb7441531fbf1f599a6ce6fd9ba40f8437f96ca7
Lorenzo <lorenzo@banana.ch>
2023-01-18 11:51:26 +0100

    Fix error adding translations to extensions

--- SampleTranslations/CMake/translations/translations_fr.ts
+++ SampleTranslations/CMake/translations/translations_fr.ts
@@ -12,7 +12,7 @@
 <context>
     <name>MyReport</name>
     <message>
-        <location filename="../main.js" line="41"/>
+        <location filename="../translations.js" line="43"/>
         <source>Account card</source>
         <translation>Fiche de compte</translation>
     </message>
@@ -20,42 +20,77 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="../main.js" line="34"/>
+        <source>Translations:</source>
+        <translation type="vanished">Traductions:</translation>
+    </message>
+    <message>
+        <source>Italian</source>
+        <translation type="vanished">Italien</translation>
+    </message>
+    <message>
+        <source>French</source>
+        <translation type="vanished">Français</translation>
+    </message>
+    <message>
+        <source>German</source>
+        <translation type="vanished">Allemand</translation>
+    </message>
+    <message>
+        <source>English</source>
+        <translation type="vanished">Anglais</translation>
+    </message>
+    <message>
+        <source>Unknow</source>
+        <translation type="vanished">Inconnue</translation>
+    </message>
+    <message>
+        <source>In the language of the document</source>
+        <translation type="vanished">Dans la langue du document</translation>
+    </message>
+    <message>
+        <source>Text</source>
+        <translation type="vanished">Text</translation>
+    </message>
+</context>
+<context>
+    <name>translations</name>
+    <message>
+        <location filename="../translations.js" line="36"/>
         <source>Translations:</source>
         <translation>Traductions:</translation>
     </message>
     <message>
-        <location filename="../main.js" line="76"/>
+        <location filename="../translations.js" line="78"/>
         <source>Italian</source>
         <translation>Italien</translation>
     </message>
     <message>
-        <location filename="../main.js" line="77"/>
+        <location filename="../translations.js" line="79"/>
         <source>French</source>
         <translation>Français</translation>
     </message>
     <message>
-        <location filename="../main.js" line="78"/>
+        <location filename="../translations.js" line="80"/>
         <source>German</source>
         <translation>Allemand</translation>
     </message>
     <message>
-        <location filename="../main.js" line="79"/>
+        <location filename="../translations.js" line="81"/>
         <source>English</source>
         <translation>Anglais</translation>
     </message>
     <message>
-        <location filename="../main.js" line="80"/>
+        <location filename="../translations.js" line="82"/>
         <source>Unknow</source>
         <translation>Inconnue</translation>
     </message>
     <message>
-        <location filename="../main.js" line="83"/>
+        <location filename="../translations.js" line="85"/>
         <source>In the language of the document</source>
         <translation>Dans la langue du document</translation>
     </message>
     <message>
-        <location filename="../main.js" line="88"/>
+        <location filename="../translations.js" line="90"/>
         <source>Text</source>
         <translation>Text</translation>
     </message>
--- SampleTranslations/CMake/translations/translations_it.ts
+++ SampleTranslations/CMake/translations/translations_it.ts
@@ -12,7 +12,7 @@
 <context>
     <name>MyReport</name>
     <message>
-        <location filename="../main.js" line="41"/>
+        <location filename="../translations.js" line="43"/>
         <source>Account card</source>
         <translation>Scheda conto</translation>
     </message>
@@ -20,42 +20,77 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="../main.js" line="34"/>
+        <source>Translations:</source>
+        <translation type="vanished">Traduzioni:</translation>
+    </message>
+    <message>
+        <source>Italian</source>
+        <translation type="vanished">Italiano</translation>
+    </message>
+    <message>
+        <source>French</source>
+        <translation type="vanished">Francese</translation>
+    </message>
+    <message>
+        <source>German</source>
+        <translation type="vanished">Tedesco</translation>
+    </message>
+    <message>
+        <source>English</source>
+        <translation type="vanished">Inglese</translation>
+    </message>
+    <message>
+        <source>Unknow</source>
+        <translation type="vanished">Sconosciuto</translation>
+    </message>
+    <message>
+        <source>In the language of the document</source>
+        <translation type="vanished">Nella lingua del documento</translation>
+    </message>
+    <message>
+        <source>Text</source>
+        <translation type="vanished">Testo</translation>
+    </message>
+</context>
+<context>
+    <name>translations</name>
+    <message>
+        <location filename="../translations.js" line="36"/>
         <source>Translations:</source>
         <translation>Traduzioni:</translation>
     </message>
     <message>
-        <location filename="../main.js" line="76"/>
+        <location filename="../translations.js" line="78"/>
         <source>Italian</source>
         <translation>Italiano</translation>
     </message>
     <message>
-        <location filename="../main.js" line="77"/>
+        <location filename="../translations.js" line="79"/>
         <source>French</source>
         <translation>Francese</translation>
     </message>
     <message>
-        <location filename="../main.js" line="78"/>
+        <location filename="../translations.js" line="80"/>
         <source>German</source>
         <translation>Tedesco</translation>
     </message>
     <message>
-        <location filename="../main.js" line="79"/>
+        <location filename="../translations.js" line="81"/>
         <source>English</source>
         <translation>Inglese</translation>
     </message>
     <message>
-        <location filename="../main.js" line="80"/>
+        <location filename="../translations.js" line="82"/>
         <source>Unknow</source>
         <translation>Sconosciuto</translation>
     </message>
     <message>
-        <location filename="../main.js" line="83"/>
+        <location filename="../translations.js" line="85"/>
         <source>In the language of the document</source>
         <translation>Nella lingua del documento</translation>
     </message>
     <message>
-        <location filename="../main.js" line="88"/>
+        <location filename="../translations.js" line="90"/>
         <source>Text</source>
         <translation>Testo</translation>
     </message>
